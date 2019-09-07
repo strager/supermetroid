8a427f2b81365b87f34ebc7b999a2789db232d3c
Matthew Glazar <strager.nds@gmail.com>
2019-09-07 16:02:58 -0700

    Label SNES ROM header
    Use wla-dx' .snesheader directive to label the ROM header.
    
    Also, replace the $ff padding before the ROM header with empty space
    using the .emptyfill directive.

--- src/common.asm
+++ src/common.asm
@@ -11,3 +11,15 @@ bankstotal $60
 banksize $8000
 banks $60
 .endro
+
+.snesheader
+name "Super Metroid        "
+cartridgetype $02 ; ROM + RAM + Battery
+romsize $0c ; 32 Megabits
+sramsize $03 ; 64 Kilobits
+country $00 ; Japan (J)
+licenseecode $01 ; Nintendo
+version $00
+.endsnes
+
+.emptyfill $ff
