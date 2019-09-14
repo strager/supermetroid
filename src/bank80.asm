b0cc7298fcdffb267505c84f5dacca6bac90da4d
Matthew Glazar <strager.nds@gmail.com>
2019-09-14 15:33:04 -0700

    Label unknown_80_971a

--- src/bank80.asm
+++ src/bank80.asm
@@ -3061,10 +3061,10 @@ unknown_80_96f1:
 
 unknown_80_971a:
   sep #$20
-  lda #$04
-  sta $212c.w
-  stz $2130.w
-  stz $2131.w
+  lda #IO_TM_BG3
+  sta IO_TM
+  stz IO_CGWSEL
+  stz IO_CGADSUB
   rep #$20
   lda #$000e.w
   ldy #$001f.w
