e62a4ff9555bc44bf428afb5172cf94b48568c03
Matthew Glazar <strager.nds@gmail.com>
2020-06-22 15:52:54 -0700

    Document unknown_81_8b22

--- src/bank81.asm
+++ src/bank81.asm
@@ -1213,64 +1213,114 @@ unknown_81_8ab8:
 
 /*unknown_81_8b21:*/ rtl
 
+; Copy sprite tiles to OAM.
+;
+; See also draw_sprite_tiles.
+;
+; Inputs:
+; * [Y]
+; * [var_temp_center_x]: X coordinate of the sprite's center.
+; * [var_temp_center_y]: Y coordinate of the sprite's center.
+; * [var_unknown_03]
+;
+; Outputs:
+; * [var_oam_objects]
+; * [var_oam_objects_tail]
+;
+; Clobbers:
+; * A
+; * X
+; * flags
 unknown_81_8b22:
   phy
-  lda 0, Y
-  beq @unknown_81_8b94
-  iny
-  iny
-  sta var_unknown_18
+  lda 0, Y ; Read tile count.
+  beq @done ; Branch if tile count == 0.
+  iny ; Skip over tile count.
+  iny ; Skip over tile count.
+  sta var_temp_number_of_tiles
   ldx var_oam_objects_tail.w
   clc
-@unknown_81_8b30:
-  lda 0, Y
-  adc var_unknown_14
+
+@draw_tile:
+  ; [X.x] := [Y.x_and_flags] + [var_temp_center_x]
+  lda sprite_tile.x_and_flags, Y
+  adc var_temp_center_x
   sta var_oam_objects.x.w, X
+
+  ; Branch to @set_extra_large_or_small if
+  ; ([.x_and_flags] + [var_temp_center_x]) & SPRITE_TILE_XAF_X_MASK <= $ff.
   and #$0100
-  beq @unknown_81_8b4b
+  beq @set_extra_large_or_small
+
+@set_extra_x8:
+  ; Set OAM_OBJ_EXTRA_X8_MASK in [var_oam_objects_extra + X/2].
   lda draw_sprite_tiles@oam_extra_address.l, X
   sta var_unknown_16
   lda (var_unknown_16)
   ora draw_sprite_tiles@oam_extra_x8_and_small.l, X
   sta (var_unknown_16)
-@unknown_81_8b4b:
-  lda 0, Y
-  bpl @unknown_81_8b5e
+
+@set_extra_large_or_small:
+  lda sprite_tile.x_and_flags, Y
+  bpl @set_extra_small ; Branch if SPRITE_TILE_XAF_SIZE is unset.
+
+@set_extra_large:
+  ; Set OAM_OBJ_EXTRA_SIZE_LARGE in [var_oam_objects_extra + X/2].
   lda draw_sprite_tiles@oam_extra_address.l, X
   sta var_unknown_16
   lda (var_unknown_16)
   ora draw_sprite_tiles@oam_extra_large.l, X
   sta (var_unknown_16)
-@unknown_81_8b5e:
+  ; Fall through to @set_y.
+
+@set_extra_small:
+  ; Do nothing. var_oam_objects_extra has already been cleared
+  ; (OAM_OBJ_EXTRA_SIZE_SMALL == 0).
+
+@set_y:
   sep #$20
-  lda 2, Y
-  bmi @unknown_81_8b6b
-  adc var_unknown_12
-  bcc @unknown_81_8b71
-  bcs @unknown_81_8b6f
-@unknown_81_8b6b:
-  adc var_unknown_12
-  bcs @unknown_81_8b71
-@unknown_81_8b6f:
-  lda #$f0
-@unknown_81_8b71:
+  lda sprite_tile.y, Y
+  bmi @y_is_negative ; Branch if [.y] < 0 (signed).
+
+  ; Assumption: [.y] >= 0
+  ; Branch to @tile_is_off_screen if [var_temp_center_y] < 0
+  ; or [.y] + [var_temp_center_y] >= 256. Otherwise, branch to @show_tile.
+  adc var_temp_center_y
+  bcc @show_tile
+  bcs @tile_is_off_screen
+
+@y_is_negative:
+  ; Assumption: [.y] < 0
+  ; Branch if [.y] + [var_temp_center_y] >= 0.
+  adc var_temp_center_y
+  bcs @show_tile
+
+@tile_is_off_screen:
+  lda #$f0 ; Put the tile off-screen.
+@show_tile:
+  ; [X.y] := [Y.y] + [var_temp_center_y] or $f0
   sta var_oam_objects.y.w, X
   rep #$21
-  lda 3, Y
+
+  ; [X.tile_and_attributes] := [Y.oam_tile_and_attributes] | [var_unknown_03]
+  lda sprite_tile.oam_tile_and_attributes, Y
   adc 0
   ora var_unknown_03
   sta var_oam_objects.tile_and_attributes.w, X
+
   tya
-  adc #$0005.w
+  adc #sprite_tile@size
   tay
+
   txa
-  adc #$0004.w
-  and #$01ff.w
+  adc #oam_obj@size
+  and #(OAM_OBJ_COUNT * oam_obj@size) - 1
   tax
-  dec var_unknown_18
-  bne @unknown_81_8b30
+
+  dec var_temp_number_of_tiles
+  bne @draw_tile
   stx var_oam_objects_tail.w
-@unknown_81_8b94:
+@done:
   ply
   rtl
 
